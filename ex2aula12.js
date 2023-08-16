29e7a11e04b7584e95a9add30866471f2f9e9b16
caroliv96 <121107004+caroliv96@users.noreply.github.com>
2023-08-16 13:43:22 -0300

    exs js

---
+++ ex2aula12.js
@@ -0,0 +1,5 @@
+var soma =0;
+for(var i=12; i<=30; i+=2){
+    soma +=i;
+    console.log(soma)
+}
---
+++ exaula12.js
@@ -0,0 +1,6 @@
+
+var soma =0;
+for(var i = 11; i<=30; i+=2){
+    soma +=i;
+    console.log(soma);
+}
